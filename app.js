0179aa76366be5adde15a94535fa50d3cf6393ef
Micah Fukazawa <micahfukazawa@gmail.com>
2020-03-10 12:57:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: "#app",
+  data: {
+    newGame: false,
+    newGameAttack: false,
+    playerHP: 100,
+    monsterHP: 100
+  },
+  methods: {
+    newGameStart: function() {
+      this.newGame = !this.newGame;
+    }
+  }
+});
